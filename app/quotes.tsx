b79ba2632848e4651f1360e7127615702cc64165
Ayahviramempesonah <git@stackblitz.com>
2024-10-02 00:38:43 +0700

    Initial commit

---
+++ app/quotes.tsx
@@ -0,0 +1,5 @@
+export default [
+  "Jangan biarkan hari kemarin menyita terlalu banyak waktu di hari ini.” — Will Rogers",
+  "Ambisi adalah menyusun tangga menuju langit.",
+  "Kegembiraan yang dibagi adalah kegembiraan yang berlipat ganda.",
+  ];
